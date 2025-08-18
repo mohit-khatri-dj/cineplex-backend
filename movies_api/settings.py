c4cc8fa43e601dca08d8e1bac4f343958e4450a2
Madhuban Khatri <madhuban211998@gmail.com>
2025-08-18 19:42:49 +0530

    update

--- movies_api/settings.py
+++ movies_api/settings.py
@@ -2,6 +2,10 @@ import os
 from pathlib import Path
 import dj_database_url
 import cloudinary_storage
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 

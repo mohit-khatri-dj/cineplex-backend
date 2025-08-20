d0d14d42e562ae5c943a97d7a826b1d4d272cfd2
Madhuban Khatri <madhuban211998@gmail.com>
2025-08-20 14:49:46 +0530

    update

--- movies_api/settings.py
+++ movies_api/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -125,7 +126,14 @@ USE_TZ = True
 # -----------------
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']  
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STORAGES = {
+    # ...
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 MEDIA_URL = '/media/' 
 MEDIA_ROOT = BASE_DIR / 'media'
